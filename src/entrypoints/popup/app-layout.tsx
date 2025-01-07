e34d3ec5fc64a5f303f98dfa764115cd635033cc
vqhdev <vqh.dev@gmail.com>
2025-01-07 16:01:28 +0700

    fix scroll content

--- src/entrypoints/popup/app-layout.tsx
+++ src/entrypoints/popup/app-layout.tsx
@@ -184,7 +184,7 @@ export default function AppLayout() {
             )}
             <div
               style={{
-                display: isLoading ? 'none' : 'block',
+                display: isLoading ? 'none' : 'initial',
               }}
             >
               <Outlet />
